41168c9967989da96b4fc3e8e8b79244d606dc1b
Enzo Mendez <enzo.mendez315@gmail.com>
2023-07-19 11:12:54 -0600

    Added getters for classes and created tester class.

--- Scheduler.cs
+++ Scheduler.cs
@@ -1,20 +1,43 @@
-﻿namespace Scheduler
+﻿namespace SchedulerBuilder
 {
     public class Scheduler
     {
         private Dictionary<string, Course> completed;
         private Dictionary<string, Course> courses;
+        private Dictionary<string, List<Course>> prerequisites;
 
         public Scheduler()
         {
             completed = new Dictionary<string, Course>();
             courses = new Dictionary<string, Course>();
+            prerequisites = new Dictionary<string, List<Course>>();
         }
 
         public Scheduler(List<string> courseList)
         {
             completed = new Dictionary<string, Course>();
             courses = new Dictionary<string, Course>();
+            prerequisites = new Dictionary<string, List<Course>>();
+        }
+
+        public Course GetCompleted(string course)
+        {
+            if (!completed.ContainsKey(course))
+            {
+                throw new KeyNotFoundException(course);
+            }
+
+            return completed[course];
+        }
+
+        public Course GetCourse(string course)
+        {
+            if (!courses.ContainsKey(course))
+            {
+                throw new KeyNotFoundException(course);
+            }
+
+            return courses[course];
         }
 
         public void AddCompleted(Course course)
@@ -40,9 +63,9 @@
         private string name;
         private string description;
 
-        public Course(int code, string name, string description)
+        public Course(string departmentCode, int courseCode, string name, string description)
         {
-            this.code = "CS " + code.ToString();
+            this.code = departmentCode + " " + courseCode.ToString();
             this.name = name;
             this.description = description;
         }
@@ -60,6 +83,11 @@
         public string Description
         {
             get { return description; }
+        }
+
+        public override string ToString()
+        {
+            return code + " - " + name;
         }
     }
 }
