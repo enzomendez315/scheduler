335bcce5c284967f4e5c2591591fe2688dd9061b
Enzo Mendez <enzo.mendez315@gmail.com>
2023-07-19 09:56:11 -0600

    Created Scheduler and Course classes.

--- Scheduler.cs
+++ Scheduler.cs
@@ -2,33 +2,64 @@
 {
     public class Scheduler
     {
-        private Dictionary<string, bool> completed;
-        private Dictionary<string, bool> courses;
+        private Dictionary<string, Course> completed;
+        private Dictionary<string, Course> courses;
 
         public Scheduler()
         {
-            completed = new Dictionary<string, bool>();
-            courses = new Dictionary<string, bool>();
+            completed = new Dictionary<string, Course>();
+            courses = new Dictionary<string, Course>();
         }
 
-        public Scheduler(List<string> courses)
+        public Scheduler(List<string> courseList)
         {
-            completed = new Dictionary<string, bool>();
-            this.courses = new Dictionary<string, bool>();
+            completed = new Dictionary<string, Course>();
+            courses = new Dictionary<string, Course>();
+        }
+
+        public void AddCompleted(Course course)
+        {
+            if (!completed.ContainsKey(course.Code))
+            {
+                completed[course.Code] = course;
+            }
+        }
+
+        public void AddCourse(Course course)
+        {
+            if (!courses.ContainsKey(course.Code))
+            {
+                courses[course.Code] = course;
+            }
         }
     }
 
     public class Course
     {
-        private string classification;
+        private string code;
         private string name;
         private string description;
 
-        public Course(int number, string name, string description)
+        public Course(int code, string name, string description)
         {
-            classification = "CS " + number.ToString();
+            this.code = "CS " + code.ToString();
             this.name = name;
             this.description = description;
+        }
+
+        public string Code
+        {
+            get { return code; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public string Description
+        {
+            get { return description; }
         }
     }
 }
