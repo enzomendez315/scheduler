95603bb04add2db34fa94c276dcccbc0318e164c
Enzo Mendez <enzo.mendez315@gmail.com>
2023-07-19 09:12:47 -0600

    Add project files.

---
+++ Scheduler.cs
@@ -0,0 +1,34 @@
+﻿namespace Scheduler
+{
+    public class Scheduler
+    {
+        private Dictionary<string, bool> completed;
+        private Dictionary<string, bool> courses;
+
+        public Scheduler()
+        {
+            completed = new Dictionary<string, bool>();
+            courses = new Dictionary<string, bool>();
+        }
+
+        public Scheduler(List<string> courses)
+        {
+            completed = new Dictionary<string, bool>();
+            this.courses = new Dictionary<string, bool>();
+        }
+    }
+
+    public class Course
+    {
+        private string classification;
+        private string name;
+        private string description;
+
+        public Course(int number, string name, string description)
+        {
+            classification = "CS " + number.ToString();
+            this.name = name;
+            this.description = description;
+        }
+    }
+}
